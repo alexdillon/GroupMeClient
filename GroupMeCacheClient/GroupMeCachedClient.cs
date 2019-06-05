6bbdef664cf56414591f9c5efcec040914fed191
Alex Dillon <6540981+alexdillon@users.noreply.github.com>
2019-06-04 23:24:13 -0500

    Created project for GroupMeClientCached

---
+++ GroupMeCacheClient/GroupMeCachedClient.cs
@@ -0,0 +1,24 @@
+﻿namespace GroupMeCacheClient
+{
+    using System;
+    using GroupMeClientApi;
+
+    /// <summary>
+    /// <see cref="GroupMeCachedClient"/> provides access to the GroupMe API backed with a local cache database.
+    /// </summary>
+    public class GroupMeCachedClient : GroupMeClient
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GroupMeCachedClient"/> class.
+        /// </summary>
+        /// <param name="authToken">The authorization token to be used for GroupMe operations.</param>
+        /// <param name="databasePath">The path to the database file.</param>
+        public GroupMeCachedClient(string authToken, string databasePath)
+            : base(authToken)
+        {
+            this.DatabasePath = databasePath ?? throw new ArgumentNullException(nameof(databasePath));
+        }
+
+        private string DatabasePath { get; set; }
+    }
+}
