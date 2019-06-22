5f5a493c0db09931b65c46fd54c6be3cc124c885
Alex Dillon <6540981+alexdillon@users.noreply.github.com>
2019-06-22 12:04:09 -0500

    Partial support for showing groups in the UI

--- GroupMeClient/ViewModels/ChatsViewModel.cs
+++ GroupMeClient/ViewModels/ChatsViewModel.cs
@@ -1,7 +1,11 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using GroupMeClientApi.Models;
 
 namespace GroupMeClient.ViewModels
 {
@@ -11,12 +15,17 @@ namespace GroupMeClient.ViewModels
         {
             ShowPopUp = new RelayCommand(() => ShowPopUpExecute(), () => true);
             IncrementValue = new RelayCommand(() => IncrementValueExecute(), () => true);
+            LoadedCommand = new RelayCommand(async () => await Loaded(), () => true);
             ExampleValue = 0;
+
+            this.ActiveGroups = new ObservableCollection<Group>();
         }
 
         public ICommand ShowPopUp { get; private set; }
 
         public ICommand IncrementValue { get; private set; }
+
+        public ICommand LoadedCommand { get; private set; }
 
         private static void ShowPopUpExecute()
         {
@@ -28,7 +37,27 @@ namespace GroupMeClient.ViewModels
             ExampleValue += 1;
         }
 
+        private async Task Loaded()
+        {
+            string token = System.IO.File.ReadAllText("../../../DevToken.txt");
+            var groupMeClient = new GroupMeClientCached.GroupMeCachedClient(token, "cache.db");
+
+            var groups = await groupMeClient.GetGroupsAsync();
+            //var messagesInFirstGroup = await groups[0].GetMessagesAsync();
+
+            //var chats = await groupMeClient.GetChatsAsync();
+            //var messagesInFirstChat = await chats[0].GetMessagesAsync();
+
+            this.ActiveGroups.Clear();
+            foreach (var group in groupMeClient.Groups)
+            {
+                this.ActiveGroups.Add((group));
+            }
+        }
+
         int _exampleValue;
+
+        public ObservableCollection<Group> ActiveGroups { get; set; }
 
         public int ExampleValue
         {
