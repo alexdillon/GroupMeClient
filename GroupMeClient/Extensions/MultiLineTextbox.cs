e89695feaa6e8a665153dd456b4e766228c7e8a6
Alex Dillon <6540981+alexdillon@users.noreply.github.com>
2019-07-02 20:34:32 -0500

    Remove WebBrowserBehavior and add MultiLineTextBox control

---
+++ GroupMeClient/Extensions/MultiLineTextbox.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace GroupMeClient.Extensions
+{
+    public class MultiLineSendBox : TextBox
+    {
+        public MultiLineSendBox()
+        {
+            this.KeyDown += TextBoxKeyDown;
+            this.PreviewKeyDown += TextBoxPreviewKeyDown;
+        }
+
+        private void TextBoxKeyDown(object sender, KeyEventArgs e)
+        {
+            // This will never happen because the Enter Key is handeled before
+            // That means TextBoxKeyDown is not triggered for the Enter key
+
+            if (e.Key == Key.Enter &&
+                !(Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl)) &&
+                !(Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.LeftShift)))
+            {
+                RaiseSendEvent();
+            }
+        }
+
+        private void TextBoxPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            // Enter key is routed and the PreviewKeyDown is also fired with the 
+            // Enter key
+            // You don't want to clear the box when CTRL and/or SHIFT is down
+            if (e.Key == Key.Enter &&
+                !(Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl)) &&
+                !(Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift)))
+            {
+                e.Handled = true;
+                RaiseSendEvent();
+            }
+        }
+
+        public static readonly RoutedEvent SendEvent = EventManager.RegisterRoutedEvent(
+         "Send", RoutingStrategy.Direct, typeof(RoutedEventHandler), typeof(MultiLineSendBox));
+
+        // Provide CLR accessors for the event
+        public event RoutedEventHandler Send
+        {
+            add { AddHandler(SendEvent, value); }
+            remove { RemoveHandler(SendEvent, value); }
+        }
+
+        // This method raises the Tap event
+        void RaiseSendEvent()
+        {
+            RoutedEventArgs newEventArgs = new RoutedEventArgs(MultiLineSendBox.SendEvent);
+            RaiseEvent(newEventArgs);
+        }
+    }
+}
