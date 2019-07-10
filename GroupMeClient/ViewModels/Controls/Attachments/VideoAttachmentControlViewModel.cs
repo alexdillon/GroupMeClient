e40428c7d7ca82446a2c94303866e552ce3717c6
Alex Dillon <6540981+alexdillon@users.noreply.github.com>
2019-07-09 22:33:00 -0500

    Added Attachment Card to handle GroupMe videos

---
+++ GroupMeClient/ViewModels/Controls/Attachments/VideoAttachmentControlViewModel.cs
@@ -0,0 +1,34 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System.Windows.Input;
+
+namespace GroupMeClient.ViewModels.Controls.Attachments
+{
+    public class VideoAttachmentControlViewModel : LinkAttachmentBaseViewModel
+    {
+        public VideoAttachmentControlViewModel(string url)
+        {
+            this.Url = url;
+
+            if (this.Uri != null && this.Uri.Host == "v.groupme.com")
+            {
+                var newUri = this.Uri.AbsoluteUri;
+                newUri = newUri.Replace(".mp4", ".jpg");
+
+                _ = DownloadImage(newUri);
+            }
+
+            this.Clicked = new RelayCommand(ClickedAction);
+        }
+
+        public ICommand Clicked { get; }
+
+        protected override void MetadataDownloadCompleted()
+        {
+        }
+
+        private void ClickedAction()
+        {
+            Extensions.WebBrowserHelper.OpenUrl(this.Url);
+        }
+    }
+}
---
+++ GroupMeClient/Views/Controls/Attachments/VideoAttachmentControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace GroupMeClient.Views.Controls.Attachments
+{
+    /// <summary>
+    /// Interaction logic for VideoAttachmentControl.xaml
+    /// </summary>
+    public partial class VideoAttachmentControl : UserControl
+    {
+        public VideoAttachmentControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
