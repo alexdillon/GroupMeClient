06dc2dfff219b3f099eda0fd9d4fb8abb1ddd02f
Alex Dillon <6540981+alexdillon@users.noreply.github.com>
2019-07-12 19:14:58 -0500

    Added generic popup template

---
+++ GroupMeClient/Views/Controls/Popup.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace GroupMeClient.Views.Controls
+{
+    /// <summary>
+    /// Interaction logic for Popup.xaml
+    /// </summary>
+    public partial class Popup : ContentControl
+    {
+        public Popup()
+        {
+            InitializeComponent();
+        }
+    }
+}
