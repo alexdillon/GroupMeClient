e3b816530bdd8d40176dc2d6174682b53d94f2ee
Alex Dillon <6540981+alexdillon@users.noreply.github.com>
2019-07-12 19:17:09 -0500

    Created SendImageControl for captioning and sending images

---
+++ GroupMeClient/ViewModels/Controls/SendImageControlViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+using GroupMeClientApi.Models;
+
+namespace GroupMeClient.ViewModels.Controls
+{
+    public class SendImageControlViewModel : GalaSoft.MvvmLight.ViewModelBase, IDisposable
+    {
+        public SendImageControlViewModel()
+        {
+        }
+
+        private string typedMessageContents;
+
+        private Message composedMessage;
+
+        private bool isSending;
+
+        public ICommand SendMessage { get; set; }
+
+        public Stream ImageStream { get; set; }
+
+        public string TypedMessageContents
+        {
+            get { return this.typedMessageContents; }
+            set { Set(() => this.TypedMessageContents, ref typedMessageContents, value); }
+        }
+
+        public bool IsSending
+        {
+            get { return this.isSending; }
+            set { Set(() => this.IsSending, ref isSending, value); }
+        }
+
+        void IDisposable.Dispose()
+        {
+            this.ImageStream.Close();
+            this.ImageStream.Dispose();
+        }
+    }
+}
---
+++ GroupMeClient/Views/Controls/SendImageControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace GroupMeClient.Views.Controls
+{
+    /// <summary>
+    /// Interaction logic for SendImageControl.xaml
+    /// </summary>
+    public partial class SendImageControl : UserControl
+    {
+        public SendImageControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
