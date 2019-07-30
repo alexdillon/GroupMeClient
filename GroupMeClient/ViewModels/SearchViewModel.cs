2ef48465d03662209f36b272e0a9df177969f7ad
Alex Dillon <6540981+alexdillon@users.noreply.github.com>
2019-07-30 00:25:28 -0500

    Rough layout for Search Page

--- GroupMeClient/ViewModels/SearchViewModel.cs
+++ GroupMeClient/ViewModels/SearchViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -15,7 +16,8 @@ namespace GroupMeClient.ViewModels
     /// </summary>
     public class SearchViewModel : ViewModelBase
     {
-        private ViewModelBase smallDialog;
+        private ViewModelBase popupDialog;
+        private string searchTerm = string.Empty;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="SearchViewModel"/> class.
@@ -27,6 +29,11 @@ namespace GroupMeClient.ViewModels
             this.GroupMeClient = groupMeClient ?? throw new System.ArgumentNullException(nameof(groupMeClient));
             this.SettingsManager = settingsManager ?? throw new ArgumentNullException(nameof(settingsManager));
 
+            this.AllGroupsChats = new ObservableCollection<GroupControlViewModel>();
+
+            this.ClosePopup = new RelayCommand(this.CloseBigPopup);
+            this.EasyClosePopup = new RelayCommand(this.CloseBigPopup);
+
             this.Loaded = new RelayCommand(async () => await this.IndexGroups(), true);
         }
 
@@ -36,13 +43,45 @@ namespace GroupMeClient.ViewModels
         public ICommand Loaded { get; private set; }
 
         /// <summary>
-        /// Gets the Small Dialog that should be displayed as a popup.
+        /// Gets a listing of all available Groups and Chats.
+        /// </summary>
+        public ObservableCollection<GroupControlViewModel> AllGroupsChats { get; }
+
+        /// <summary>
+        /// Gets the action to be be performed when the big popup has been closed.
+        /// </summary>
+        public ICommand ClosePopup { get; }
+
+        /// <summary>
+        /// Gets the action to be be performed when the big popup has been closed indirectly.
+        /// This typically is from the user clicking in the gray area around the popup to dismiss it.
+        /// </summary>
+        public ICommand EasyClosePopup { get; }
+
+        /// <summary>
+        /// Gets the Big Dialog that should be displayed as a popup.
         /// Gets null if no dialog should be displayed.
         /// </summary>
-        public ViewModelBase SmallDialog
+        public ViewModelBase PopupDialog
         {
-            get { return this.smallDialog; }
-            private set { this.Set(() => this.SmallDialog, ref this.smallDialog, value); }
+            get { return this.popupDialog; }
+            private set { this.Set(() => this.PopupDialog, ref this.popupDialog, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the string entered to search for.
+        /// </summary>
+        public string SearchTerm
+        {
+            get
+            {
+                return this.searchTerm;
+            }
+
+            set
+            {
+                this.Set(() => this.SearchTerm, ref this.searchTerm, value);
+            }
         }
 
         private GroupMeCachedClient GroupMeClient { get; }
@@ -54,7 +93,7 @@ namespace GroupMeClient.ViewModels
             try
             {
                 var loadingDialog = new LoadingControlViewModel();
-                this.SmallDialog = loadingDialog;
+                this.PopupDialog = loadingDialog;
 
                 // disable automatic database commits to improve performance
                 this.GroupMeClient.DatabaseUpdatingEnabled = false;
@@ -64,13 +103,22 @@ namespace GroupMeClient.ViewModels
 
                 var groupsAndChats = Enumerable.Concat<IMessageContainer>(groups, chats);
 
+                this.AllGroupsChats.Clear();
+
                 foreach (var group in groupsAndChats)
                 {
                     loadingDialog.Message = $"Indexing {group.Name}";
                     await this.IndexGroup(group);
+
+                    // Add Group/Chat to the list
+                    var vm = new GroupControlViewModel(group)
+                    {
+                        GroupSelected = new RelayCommand<GroupControlViewModel>(this.OpenNewGroupChat, (g) => true),
+                    };
+                    this.AllGroupsChats.Add(vm);
                 }
 
-                this.SmallDialog = null;
+                this.PopupDialog = null;
             }
             catch (Exception)
             {
@@ -110,9 +158,65 @@ namespace GroupMeClient.ViewModels
             groupState.LastFullyIndexedId = newestMessages.First().Id; // everything is downloaded
             this.SettingsManager.SaveSettings();
 
-            // restart the context to "dump" old messages out of memory
-            // and reset the DbContext internal state.
             await this.GroupMeClient.ForceUpdateAsync();
+        }
+
+        private void OpenNewGroupChat(GroupControlViewModel group)
+        {
+            //if (this.ActiveGroupsChats.Any(g => g.Id == group.Id))
+            //{
+            //    // this group or chat is already open, we just need to move it to the front
+            //    var openGroup = this.ActiveGroupsChats.First(g => g.Id == group.Id);
+            //    var indexOpenGroup = this.ActiveGroupsChats.IndexOf(openGroup);
+            //    this.ActiveGroupsChats.Move(indexOpenGroup, 0);
+            //}
+            //else
+            //{
+            //    // open a new group or chat
+            //    var groupContentsDisplay = new GroupContentsControlViewModel(group.MessageContainer)
+            //    {
+            //        CloseGroup = new RelayCommand<GroupContentsControlViewModel>(this.CloseChat),
+            //    };
+
+            //    this.ActiveGroupsChats.Insert(0, groupContentsDisplay);
+
+            //    _ = this.PushClient.SubscribeAsync(group.MessageContainer);
+
+            //    // mark all messages as read
+            //    var groupChatState = this.SettingsManager.ChatsSettings.GroupChatStates.Find(g => g.GroupOrChatId == group.Id);
+            //    groupChatState.LastTotalMessageCount = group.MessageContainer.TotalMessageCount;
+            //    groupChatState.LastReadMessageId = group.MessageContainer.LatestMessage.Id;
+
+            //    // clear the notification bubble
+            //    group.TotalUnreadCount = 0;
+
+            //    this.SettingsManager.SaveSettings();
+            //}
+
+            //// limit to three multi-chats at a time
+            //while (this.ActiveGroupsChats.Count > 3)
+            //{
+            //    var removeGroup = this.ActiveGroupsChats.Last();
+            //    this.PushClient.Unsubscribe(group.MessageContainer);
+
+            //    this.ActiveGroupsChats.Remove(removeGroup);
+            //}
+        }
+
+        private void CloseBigPopup()
+        {
+            if (this.PopupDialog is LoadingControlViewModel)
+            {
+                // Can't cancel a loading screen.
+                return;
+            }
+
+            if (this.PopupDialog is IDisposable d)
+            {
+                d.Dispose();
+            }
+
+            this.PopupDialog = null;
         }
     }
 }
