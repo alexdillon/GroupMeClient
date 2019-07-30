226f7670f1f6f15eb6c7d0e023ac51caec18a139
Alex Dillon <6540981+alexdillon@users.noreply.github.com>
2019-07-29 23:26:28 -0500

    Added support for automatically focusing the Groups/Chats filter box when the search button is clicked

---
+++ GroupMeClient/Extensions/EventFocusAttachment.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace GroupMeClient.Extensions
+{
+    /// <summary>
+    /// <see cref="EventFocusAttachment"/> provides support for assigning focus after a <see cref="ToggleButton"/> is clicked.
+    /// </summary>
+    /// <remarks>
+    /// Adapted from https://stackoverflow.com/a/2212927.
+    /// </remarks>
+    public class EventFocusAttachment
+    {
+        /// <summary>
+        /// Gets a property indicating which element should be focused when the <see cref="ToggleButton"/> is activated.
+        /// </summary>
+        public static readonly DependencyProperty ElementToFocusProperty =
+            DependencyProperty.RegisterAttached(
+              "ElementToFocus",
+              typeof(Control),
+              typeof(EventFocusAttachment),
+              new UIPropertyMetadata(null, ElementToFocusPropertyChanged));
+
+        /// <summary>
+        /// Gets a value indicating which control will be focused.
+        /// </summary>
+        /// <param name="button">The dependency object to retreive the property from.</param>
+        /// <returns>The control that will receive focus.</returns>
+        public static Control GetElementToFocus(ToggleButton button)
+        {
+            return (Control)button.GetValue(ElementToFocusProperty);
+        }
+
+        /// <summary>
+        /// Sets a value indicating which control will receive focus.
+        /// </summary>
+        /// <param name="button">The dependency object to apply the property to.</param>
+        /// <param name="value">The control that will receive focus.</param>
+        public static void SetElementToFocus(ToggleButton button, Control value)
+        {
+            button.SetValue(ElementToFocusProperty, value);
+        }
+
+        private static void ElementToFocusPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (sender is ToggleButton button)
+            {
+                button.Click += (s, args) =>
+                {
+                    Control control = GetElementToFocus(button);
+                    if (control != null)
+                    {
+                        control.Focus();
+                    }
+                };
+            }
+        }
+    }
+}
--- GroupMeClient/Extensions/IsZeroConverter.cs
+++ GroupMeClient/Extensions/IsZeroConverter.cs
@@ -4,14 +4,19 @@ using System.Windows.Data;
 
 namespace GroupMeClient.Extensions
 {
+    /// <summary>
+    /// <see cref="IsZeroConverter"/> provides a converter between Zero, Null, and Empty Strings (true), and any other value (false).
+    /// </summary>
     public class IsZeroConverter : IValueConverter
     {
+        /// <inheritdoc/>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var valStr = value.ToString();
             return valStr == "0" || string.IsNullOrEmpty(valStr);
         }
 
+        /// <inheritdoc/>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new InvalidOperationException("IsZeroConverter can only be used OneWay.");
