a12c1f9b77f23cb1d7914c729a6efbdce916b308
Alex Dillon <6540981+alexdillon@users.noreply.github.com>
2019-07-29 23:04:56 -0500

    Added LoadingControl

---
+++ GroupMeClient/ViewModels/Controls/LoadingControlViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GroupMeClient.ViewModels.Controls
+{
+    /// <summary>
+    /// <see cref="LoadingControlViewModel"/> provides a ViewModel for the <see cref="Views.Controls.LoadingControl"/> control.
+    /// </summary>
+    public class LoadingControlViewModel : GalaSoft.MvvmLight.ViewModelBase
+    {
+        private string message;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoadingControlViewModel"/> class.
+        /// </summary>
+        public LoadingControlViewModel()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the progress message.
+        /// </summary>
+        public string Message
+        {
+            get { return this.message; }
+            set { this.Set(() => this.Message, ref this.message, value); }
+        }
+    }
+}
