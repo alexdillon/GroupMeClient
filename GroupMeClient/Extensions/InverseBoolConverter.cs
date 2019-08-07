2134460535dbe3381af1b6986fd13ec08320d63d
Alex Dillon <6540981+alexdillon@users.noreply.github.com>
2019-08-07 01:41:51 -0500

    Support for a Scroll-To-Bottom button

---
+++ GroupMeClient/Extensions/InverseBoolConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace GroupMeClient.Extensions
+{
+    /// <summary>
+    /// <see cref="InverseBoolConverter"/> provides a converter for boolean logical NOT.
+    /// </summary>
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class InverseBoolConverter : IValueConverter
+    {
+        /// <inheritdoc/>
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        /// <inheritdoc/>
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ GroupMeClient/Extensions/ListBoxWithPosition.cs
@@ -0,0 +1,69 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+
+namespace GroupMeClient.Extensions
+{
+    /// <summary>
+    /// <see cref="ListBoxWithPosition"/> provides a ListBox control that reports on scroll position.
+    /// </summary>
+    public class ListBoxWithPosition : ListBox
+    {
+        /// <summary>
+        /// Gets a Dependency Property Key for the IsNotAtBottom Property.
+        /// </summary>
+        internal static readonly DependencyPropertyKey IsNotAtBottomPropertyKey =
+            DependencyProperty.RegisterReadOnly(
+                "IsNotAtBottom",
+                typeof(bool),
+                typeof(ListBoxWithPosition),
+                new PropertyMetadata(false));
+
+#pragma warning disable SA1202 // Elements should be ordered by access. Initialization for IsNotAtBottomProperty depends on IsNotAtBottomPropertyKey.
+        /// <summary>
+        /// Gets a Dependency Property indicating whether the ListBox is scrolled to the bottom.
+        /// </summary>
+        public static readonly DependencyProperty IsNotAtBottomProperty =
+            IsNotAtBottomPropertyKey.DependencyProperty;
+#pragma warning restore SA1202 // Elements should be ordered by access
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ListBoxWithPosition"/> class.
+        /// </summary>
+        public ListBoxWithPosition()
+        {
+            this.Loaded += this.ListBoxWithPosition_Loaded;
+            this.ScrollToEnd = new RelayCommand(this.DoScrollToEnd);
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the ListBox is scrolled to the bottom.
+        /// </summary>
+        public bool IsNotAtBottom => (bool)this.GetValue(IsNotAtBottomProperty);
+
+        /// <summary>
+        /// Gets a command that can be executed to scroll this <see cref="ListBox"/> to the bottom.
+        /// </summary>
+        public ICommand ScrollToEnd { get; }
+
+        private ScrollViewer ScrollViewer { get; set; }
+
+        private void ListBoxWithPosition_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.ScrollViewer = ListBoxExtensions.FindSimpleVisualChild<ScrollViewer>(this);
+            this.ScrollViewer.ScrollChanged += this.ScrollViewer_ScrollChanged;
+        }
+
+        private void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            var atBottom = e.VerticalOffset == (e.OriginalSource as ScrollViewer).ScrollableHeight;
+            this.SetValue(IsNotAtBottomPropertyKey, !atBottom);
+        }
+
+        private void DoScrollToEnd()
+        {
+            this.ScrollViewer.ScrollToEnd();
+        }
+    }
+}
