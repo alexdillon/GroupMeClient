35594bfde2d715c3785db9991dbc6cce68779aff
Alex Dillon <6540981+alexdillon@users.noreply.github.com>
2019-09-15 01:00:49 -0500

    Added support for automatically assigning a version number from a git tag

---
+++ GroupMeClient/GlobalAssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+// AssemblyVersion = full version info, major.minor.patch
+[assembly: AssemblyVersion(ThisAssembly.SimpleVersion)]
+
+// FileVersion = full version info, major.minor.patch
+[assembly: AssemblyFileVersion(ThisAssembly.SimpleVersion)]
+
+// InformationalVersion = full version + branch + commit sha.
+[assembly: AssemblyInformationalVersion(ThisAssembly.InformationalVersion)]
+
+/// <summary>
+/// Defines Version Numbers used for automatic assembly versioning.
+/// </summary>
+public partial class ThisAssembly
+{
+    /// <summary>
+    /// Simple release-like version number, like 4.0.1.
+    /// </summary>
+    public const string SimpleVersion = Git.BaseVersion.Major + "." + Git.BaseVersion.Minor + "." + Git.BaseVersion.Patch;
+
+    /// <summary>
+    /// Full version, plus branch and commit short sha, like 4.0.1-39cf84e-branch.
+    /// </summary>
+    public const string InformationalVersion = SimpleVersion + "-" + Git.Commit + "+" + Git.Branch;
+}
