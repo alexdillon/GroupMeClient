da8794e0805b6b44ae6fc639a718084d940109cb
Alex Dillon <6540981+alexdillon@users.noreply.github.com>
2019-10-03 09:50:57 -0500

    Remove old code

--- GroupMeClientApi/AllowCache.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("GroupMeClientCached")]
-
-namespace GroupMeClientApi
-{
-    // This is a do-nothing file to allow the Cache Client to be able to access Internals
-}
