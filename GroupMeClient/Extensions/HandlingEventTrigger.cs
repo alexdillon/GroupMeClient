57b229aa8962ab030df86604149a9b83fa8aabc0
Alex Dillon <6540981+alexdillon@users.noreply.github.com>
2020-07-16 21:51:02 -0500

    Fixed an issue where the group information display could easily be closed by accident

---
+++ GroupMeClient/Extensions/HandlingEventTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GroupMeClient.Extensions
+{
+    /// <summary>
+    /// <see cref="HandlingEventTrigger"/> is an event trigger behavior that ensures the source event is marked
+    /// as handled to prevent it from bubbling up to high-level controls in the visual tree.
+    /// </summary>
+    public class HandlingEventTrigger : Microsoft.Xaml.Behaviors.EventTrigger
+    {
+        /// <inheritdoc/>
+        protected override void OnEvent(System.EventArgs eventArgs)
+        {
+            var routedEventArgs = eventArgs as RoutedEventArgs;
+            if (routedEventArgs != null)
+            {
+                routedEventArgs.Handled = true;
+            }
+
+            base.OnEvent(eventArgs);
+        }
+    }
+}
