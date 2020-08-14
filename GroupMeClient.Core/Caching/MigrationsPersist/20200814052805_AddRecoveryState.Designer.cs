0393c05e50f08da72dade0b66472c663e6c31f96
Alex Dillon <6540981+alexdillon@users.noreply.github.com>
2020-08-14 00:40:03 -0500

    Added migration for recovery state in persist.db

--- GroupMeClient.Core/Caching/MigrationsPersist/20200814052805_AddRecoveryState.Designer.cs
+++ GroupMeClient.Core/Caching/MigrationsPersist/20200814052805_AddRecoveryState.Designer.cs
@@ -2,14 +2,16 @@
 using GroupMeClient.Core.Caching;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GroupMeClient.Core.Caching.MigrationsPersist
 {
     [DbContext(typeof(PersistManager.PersistContext))]
-    partial class PersistContextModelSnapshot : ModelSnapshot
+    [Migration("20200814052805_AddRecoveryState")]
+    partial class AddRecoveryState
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
