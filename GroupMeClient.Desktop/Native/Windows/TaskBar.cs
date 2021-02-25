aa1b0437373e3bfd5a7f7119475f67fe8b73d114
Alex Dillon <6540981+alexdillon@users.noreply.github.com>
2021-02-24 22:36:03 -0600

    Resolves issues with taskbar shortcuts not working after updates are applied

---
+++ GroupMeClient.Desktop/Native/Windows/TaskBar.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GroupMeClient.Desktop.Native.Windows
+{
+    public class TaskBar
+    {
+        [DllImport("shell32.dll", SetLastError = true)]
+        public static extern void SetCurrentProcessExplicitAppUserModelID([MarshalAs(UnmanagedType.LPWStr)] string AppID);
+    }
+}
