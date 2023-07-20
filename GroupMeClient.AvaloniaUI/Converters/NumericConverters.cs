d88c86a95c0398170f3c0d0a9bd199af34b13079
Alex Dillon <6540981+alexdillon@users.noreply.github.com>
2023-07-19 20:13:00 -0500

    Added UI support for multiple image sending

---
+++ GroupMeClient.AvaloniaUI/Converters/NumericConverters.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Avalonia.Data.Converters;
+
+namespace GroupMeClient.AvaloniaUI.Converters
+{
+    /// <summary>
+    /// Provides a set of useful <see cref="IValueConverter"/>s for working with numeric values.
+    /// </summary>
+    public static class NumericConverters
+    {
+        /// <summary>
+        /// A value converter that returns <c>true</c> if the input value is <c>1</c>, or <c>false</c> otherwise.
+        /// </summary>
+        public static readonly IValueConverter IsOneConverter =
+            new FuncValueConverter<int?, bool>(i => i == 1);
+
+        /// <summary>
+        /// A value converter that returns <c>false</c> if the input value is <c>1</c>, or <c>true</c> otherwise.
+        /// </summary>
+        public static readonly IValueConverter IsNotOneConverter =
+            new FuncValueConverter<int, bool>(i => i != 1);
+
+        /// <summary>
+        /// A value converter that the input <see cref="int"/> value, incremented by <c>1</c>.
+        /// </summary>
+        public static readonly IValueConverter PlusOneConverter =
+            new FuncValueConverter<int, int>(i => i + 1);
+    }
+}
